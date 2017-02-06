cd60e110981e8423b40dd13fb0aa35b47cb0046d
Zoltan Biber <zoltan.biber@gmail.com>
2017-02-06 10:45:47 +0100

    Fix inconsistent chart height.
    First (temperature) chart was higher then the rest.

--- src/components/chart.js
+++ src/components/chart.js
@@ -5,7 +5,7 @@ import { Sparklines, SparklinesLine } from 'react-sparklines';
 export default (props) => {
   return (
     <div>
-      <Sparklines height={120} width={180} data={props.data}>
+      <Sparklines svgWidth={260} svgHeight={120} data={props.data}>
         <SparklinesLine color={props.color} />
       </Sparklines>
     </div>
