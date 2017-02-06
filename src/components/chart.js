115dc01cd57305c28ee31f6c29bfac45cdbf8332
Zoltan Biber <zoltan.biber@gmail.com>
2017-02-06 11:33:49 +0100

    * Fixing row height issues with CSS instead. * Adding numerical values.

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,20 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
+
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
 
 export default (props) => {
   return (
     <div>
-      <Sparklines svgWidth={260} svgHeight={120} data={props.data}>
+      <Sparklines width={260} height={120} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>{average(props.data)} {props.units}</div>
     </div>
   );
 };
